6eed4cf372078d5d3d10e0053832bd99d6a3b7c7
Frank Mutethia <50831575+frankmutethia@users.noreply.github.com>
2025-11-18 23:09:16 +0300

    Feature: postsModel.

---
+++ models/postsModel.js
@@ -0,0 +1,22 @@
+const mongoose= require('mongoose');
+
+const postsSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required: [true, 'title is required'],
+        trim: true,
+    },
+    description:{
+    type: String,
+    required: [true, 'description is required'],
+    },
+    userId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    }
+},
+{timestamps: true}
+)
+
+module.exports= mongoose.model("Post", postSchema);
